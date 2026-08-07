cce59b3ea6c71cf815bcaa59b6d7c3ab165170dd
karthik raja <37881042+mkraja826@users.noreply.github.com>
2026-08-08 00:54:59 +0530

    Export notification domain contracts

---
+++ app/notifications/__init__.py
@@ -0,0 +1,25 @@
+from app.notifications.models import (
+    NotificationDevice,
+    NotificationDeviceOverview,
+    NotificationDeviceRegistrationRequest,
+    NotificationDestination,
+    NotificationEnvelope,
+    NotificationPlatform,
+    NotificationTransportDisposition,
+    NotificationTransportKind,
+    NotificationTransportResult,
+    ReminderNotificationDispatchResult,
+)
+
+__all__ = [
+    "NotificationDevice",
+    "NotificationDeviceOverview",
+    "NotificationDeviceRegistrationRequest",
+    "NotificationDestination",
+    "NotificationEnvelope",
+    "NotificationPlatform",
+    "NotificationTransportDisposition",
+    "NotificationTransportKind",
+    "NotificationTransportResult",
+    "ReminderNotificationDispatchResult",
+]
